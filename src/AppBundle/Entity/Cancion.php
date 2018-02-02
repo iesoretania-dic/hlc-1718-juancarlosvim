7935e1c6ab30b1a8b37cf9a5c5fd5e5d2c5ae0c8
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-02-02 12:24:30 +0100

    Creada dos entidades Usuario y Cancion con las anotaciones correspondientes al ORM

---
+++ src/AppBundle/Entity/Cancion.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alumno
+ * Date: 2/02/18
+ * Time: 10:47
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Cancion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $artista;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $titulo;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $duracion:
+    /**
+     * @ORM\Column(type="date")
+     *
+     *@var \DateTime
+     */
+    private $fechaCancion;
+    /**
+     * @ORM\Column(type="string")
+     *
+     *@var string
+     */
+    private $genero;
+     /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getArtista()
+    {
+        return $this->artista;
+    }
+    /**
+     * @param mixed $artista
+     */
+    public function setArtista($artista)
+    {
+        $this->artista = $artista;
+    }
+    /**
+     * @return mixed
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+    /**
+     * @param mixed $titulo
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+    }
+    /**
+     * @return mixed
+     */
+    public function getDuracion()
+    {
+        return $this->duracion;
+    }
+    /**
+     * @param mixed $duracion
+     */
+    public function setDuracion($duracion)
+    {
+        $this->duracion = $duracion;
+    }
+    /**
+     * @return mixed
+     */
+    public function getFechaCancion()
+    {
+        return $this->fechaCancion;
+    }
+    /**
+     * @param mixed $fechaCancion
+     */
+    public function setFechaCancion($fechaCancion)
+    {
+        $this->fechaCancion = $fechaCancion;
+    }
+    /**
+     * @return mixed
+     */
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+    /**
+     * @param mixed $genero
+     */
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+    }
+
+}
---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alumno
+ * Date: 2/02/18
+ * Time: 10:30
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Usuario
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     *@var int
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $nombreUsuario;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $password;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $nombre;
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $apellidos;
+    /**
+     * @ORM\Column(type="date")
+     *
+     * @var \DateTime
+     */
+    private $fechaNacimiento
+    /**
+     * @ORM\Column(type="string")
+     *
+     *@var string
+     */
+    private $correo;
+    /**
+     * @ORM\Column(type="boolean")
+     *
+     *@var boolean
+     */
+    private $administrador;
+    /**
+     * @ORM\Column(type="boolean")
+     *
+     * @var boolean
+     */
+    private $usuarioVip;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getNombreUsuario()
+    {
+        return $this->nombreUsuario;
+    }
+    /**
+     * @param mixed $nombreUsuario
+     */
+    public function setNombreUsuario($nombreUsuario)
+    {
+        $this->nombreUsuario = $nombreUsuario;
+    }
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    /**
+     * @return mixed
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+    /**
+     * @param mixed $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+    $this->apellidos = $apellidos;
+    }/**
+     * @return mixed
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }/**
+     * @param mixed $fechaNacimiento
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+    }/**
+     * @return mixed
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }/**
+     * @param mixed $correo
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+    }/**
+     * @return mixed
+     */
+    public function getAdministrador()
+    {
+        return $this->administrador;
+    }/**
+     * @param mixed $administrador
+     */
+    public function setAdministrador($administrador)
+    {
+        $this->administrador = $administrador;
+    }/**
+     * @return mixed
+     */
+    public function getUsuarioVip()
+    {
+        return $this->usuarioVip;
+    }/**
+     * @param mixed $usuarioVip
+     */
+    public function setUsuarioVip($usuarioVip)
+    {
+        $this->usuarioVip = $usuarioVip;
+    }
+
+
+    }
