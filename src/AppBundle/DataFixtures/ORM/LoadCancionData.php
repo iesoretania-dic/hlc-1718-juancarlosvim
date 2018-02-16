c36981664d37d65b629978f83cb4172550f22e4e
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-02-16 10:25:24 +0100

    generada las 20 cancion utilizando nelmio\alice url: https://github.com/nelmio/alice#installation para instalarlo: composer require --dev nelmio/alice Nota: utilizar la version ^2.1 ya que por defecto te instala la ^3.1

--- src/AppBundle/DataFixtures/ORM/LoadCancionData.php
+++ src/AppBundle/DataFixtures/ORM/LoadCancionData.php
@@ -2,41 +2,15 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-
-use AppBundle\Entity\Cancion;
 use Doctrine\Bundle\FixturesBundle\ORMFixtureInterface;
-
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
 
 class LoadCancionData implements ORMFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // creamos una nueva idea simplemente creando una instancia de la entidad y diciéndole al
-        // EntityManager que la supervise
-        $cancion = new Cancion();
-        $cancion2 = new Cancion();
+        Fixtures::load(__DIR__.'/fixtures.yml', $manager);
 
-        $manager->persist($cancion);
-        $manager->persist($cancion2);
-
-        // rellenamos sus datos usando los setters (al ser fluent, se pueden poner en cascada)
-        $cancion
-            ->setGenero('rock')
-            ->setDuracion('3min')
-            ->setFechaCancion(new \DateTime())
-            ->setTitulo('titulo')
-            ->setArtista('artista');
-        $cancion2
-            ->setGenero('rock')
-            ->setDuracion('3min')
-            ->setFechaCancion(new \DateTime())
-            ->setTitulo('titulo2')
-            ->setArtista('artista');
-
-
-
-        // decimos al EntityManager que queremos guardar todos los cambios hechos en las entidades
-        $manager->flush();
     }
 }
